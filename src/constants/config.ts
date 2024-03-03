c0e1703b1830bf13c15f53ee021aca45b8b955f0
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2024-03-04 04:34:51 +0800

    main files

---
+++ src/constants/config.ts
@@ -0,0 +1,82 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: "Marxfolio",
+    fullName: "Wilmarx John Cayabyab",
+    email: "wilmarx.cayabyab@gmail.com",
+  },
+  hero: {
+    name: "Wilmarx John",
+    p: ["I am a Systems and Web Developer, as well as an Information Technology Personnel"],
+  },
+  contact: {
+    p: "Get in touch",
+    h2: "Contact.",
+    form: {
+      name: {
+        span: "Your Name",
+        placeholder: "What's your name?",
+      },
+      email: { span: "Your Email", placeholder: "What's your email?" },
+      message: {
+        span: "Your Message",
+        placeholder: "What do you want to say?",
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: "Introduction",
+      h2: "Overview.",
+      content: `I'm Wilmarx, an experienced IT, Systems, and Web Developer with a passion for crafting innovative solutions that streamline processes and enhance user experiences. With a strong background in software development, system administration, and web technologies, I thrive in dynamic environments where I can leverage my expertise to create robust and scalable solutions. Whether it's designing efficient systems, developing cutting-edge web applications, or optimizing IT infrastructure, I'm committed to delivering excellence and driving impactful results.`,
+    },
+    experience: {
+      p: "What I have done so far",
+      h2: "Work Experience.",
+    },
+    works: {
+      p: "Featured Projects",
+      h2: "Projects.",
+      content: ` As a dedicated Web and System Developer, I specialize in conceptualizing, designing, and implementing innovative solutions that cater to both user experience and system efficiency. With a diverse portfolio of projects under my belt, I've had the privilege of crafting dynamic websites, scalable web applications, and robust system architectures. My expertise spans front-end development, back-end programming, database management, and system integration, allowing me to deliver comprehensive solutions that meet the unique needs of each project. From e-commerce platforms to enterprise-level systems, I bring a blend of creativity, technical proficiency, and problem-solving skills to every endeavor, ensuring optimal performance and user satisfaction.`,
+    },
+  },
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,279 @@
+import type {
+  TNavLink,
+  TService,
+  TTechnology,
+  TExperience,
+  TTestimonial,
+  TProject,
+} from "../types";
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  Nexcent,
+  Logoipsum,
+  Invoice,
+  DashboardApp,
+  Dems
+} from "../assets";
+
+export const navLinks: TNavLink[] = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services: TService[] = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "System Develoer",
+    icon: mobile,
+  },
+  {
+    title: "System Administration",
+    icon: backend,
+  },
+  {
+    title: "IT Specialist",
+    icon: creator,
+  },
+];
+
+const technologies: TTechnology[] = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences: TExperience[] = [
+  {
+    title: "Chief Technological Officer",
+    companyName: "SysGo",
+    icon: backend,
+    iconBg: "#E6DEDD",
+    date: "Aug 2023 - Present",
+    points: [
+      "Developing and maintain web applications using related technologies.",
+      "Collaborate with the team members to establish objectives and design more functional, cohesive codes to enhance the user experience.",
+      "Developing ideas for new programs, products, or features by monitoring industry developments and trends.",
+      "Recording data and reporting it to proper parties, such as clients or leadership.",
+      "Participating in continuing education and training to remain current on best practices, learn new programming languages, and better assist other team members.",
+      "Get feedback from, and build solutions for, users and customers",
+      "Create quality mockups and prototypes",
+      "Stay up-to-date on emerging technologies",
+      "Write functional requirement documents and guides",
+    ],
+  },
+  {
+    title: "IT SPECIALIST",
+    companyName: "Healthier Fit Co. LTD.",
+    icon: backend,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - March 2023",
+    points: [
+      "Provided direction and motivation for IT Department team members to meet organizational needs. Provided support and services related to software, hardware, networks, systems, and servers, keeping up with the",
+      "Provide support and services related to software, hardware, networks, systems, servers, keeping up with the latest technology and provided solutions to problems the organization faced.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Educating coworkers about network security and best practices for computer usage.",
+      "Supporting day-to-day operations of the who IT Infrastructure",
+      "Install and Configure Networks",
+      "Provide User Support",
+      "Provide User Training",
+      "Troubleshoot Systems, Hardware and User Workstations",
+      "Installing new software and hardware components.",
+      "Regularly evaluating IT systems to ensure they meet the necessary demands."
+    ],
+  },
+  {
+    title: "Freelance Web Developer",
+    companyName: "",
+    icon: backend,
+    iconBg: "#383E56",
+    date: "2020 - Present",
+    points: [
+      "Developing and maintain web applications using related technologies.",
+      "Collaborate with the team members to establish objectives and design more functional, cohesive codes to enhance the user experience.",
+      "Developing ideas for new programs, products, or features by monitoring industry developments and trends.",
+      "Recording data and reporting it to proper parties, such as clients or leadership.",
+      "Participating in continuing education and training to remain current on best practices, learn new programming languages, and better assist other team members.",
+      "Get feedback from, and build solutions for, users and customers",
+    ],
+  },
+];
+
+const testimonials: TTestimonial[] = [
+];
+
+const projects: TProject[] = [
+  {
+    name: "SysGo-Dems",
+    description:
+      "Designing, Development, and Implementation of an Efficient and User-Friendly Dental Management System",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "js",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "jquery",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "php",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "bs5",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Dems,
+    sourceCodeLink: "https://github.com/SYSGO-SOLUTIONS/DEMS",
+  },
+  {
+    name: "Nexcent",
+    description:
+      "Nexcent is a figma design created by Muntasir Billah, in which I developed into a Website.",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "js",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "bs5",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: Nexcent,
+    sourceCodeLink: "https://zhackdtech.github.io/nexcent/",
+  },
+  {
+    name: "Logo Ipsum",
+    description:
+      "Outcome-centered products that reduce churn, optimize pricing, and grow your subscription business end-to-end.",
+    tags: [
+      {
+        name: "html",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
+      },
+      {
+        name: "js",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "bs5",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: Logoipsum,
+    sourceCodeLink: "https://zhackdtech.github.io/logo-ipsum/",
+  },
+  {
+    name: "Invoice App",
+    description:
+      "Invoice App Dashboard is a sample user interface that would introduce beginners to make advance charts, dashboards and User Interface for the same application types.",
+    tags: [
+      {
+        name: "Csharp(C#)",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: Invoice,
+    sourceCodeLink: "https://github.com/zhackdtech/Invoice-App",
+  },
+  
+  {
+    name: "Dashboard App",
+    description:
+      "Invoice App Dashboard is a sample user interface that would introduce beginners to make advance charts, dashboards and User Interface for the same application types.",
+    tags: [
+      {
+        name: "Csharp(C#)",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: DashboardApp,
+    sourceCodeLink: "https://github.com/zhackdtech/DASHBOARD-UI",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
